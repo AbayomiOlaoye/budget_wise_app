2847f1a011f787b606bb9011b54eb5a2a0a807da
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-03-11 21:40:21 +0100

    Set validations and associations in models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,16 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_one_attached :icon
+  has_many :category_records
+  has_many :records, through: :category_records
+
+  validates :name, presence: true, length: { maximum: 250 }
+
+  def recent_records
+    records.order(created_at: :DESC)
+  end
+
+  def recent_categories
+    categories.order(created_at: :DESC)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :categories
+  has_many :records
+
+  validates :name, presence: true, length: { maximum: 250 }
 end
