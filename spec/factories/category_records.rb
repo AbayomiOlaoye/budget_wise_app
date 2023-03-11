eab49009061b785930a07606f336aa1388db676a
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-03-11 21:32:31 +0100

    Create other migration files

---
+++ spec/factories/category_records.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :category_record do
+    category { nil }
+    record { nil }
+  end
+end
---
+++ spec/factories/records.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :record do
+    user { nil }
+    name { "MyString" }
+    amount { 1 }
+  end
+end
