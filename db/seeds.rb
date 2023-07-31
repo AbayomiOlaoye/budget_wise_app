39d5fca10a692ed0a0be4e846789da0a3eab91d6
Olaoye Oluwatoyin Abayomi <mindprepopps@gmail.com>
2023-07-31 21:50:08 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,5 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   movies_1 = Movie.create([{ name: "Star Wars2" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
